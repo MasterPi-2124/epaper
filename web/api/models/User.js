4675bf724c061becba886004bb58902e0407f856
Master Pi <minh.vln140501@gmail.com>
2023-11-10 00:05:41 +0000

    design style

--- web/api/models/User.js
+++ web/api/models/User.js
@@ -29,6 +29,12 @@ const userSchema = new Schema({
     activeTimestamp: {
         type: [String],
         required: false,
+    },
+    fontStyle: {
+        type: String,
+    },
+    desginSchema: {
+        type: String,
     }
 });
 
