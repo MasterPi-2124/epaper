0aa3f7ea768abca87926e13976ce8c4094b062a8
Master Pi <minh.vln140501@gmail.com>
2023-11-27 11:59:19 +0700

    ignore dashboard

--- web/ui/pages/dashboard/classes.js
+++ web/ui/pages/dashboard/classes.js
@@ -1,45 +1,45 @@
 import Link from "next/link";
 import Layout from "@/components/layout";
-import Menu from "@/components/dashboard/menu";
+// import Menu from "@/components/dashboard/menu";
 
-import data from "@/assets/data/dashboard.json";
+// import data from "@/assets/data/dashboard.json";
 import React, { useState, useEffect } from "react";
-import { instanceCoreApi } from "@/services/setupAxios";
+// import { instanceCoreApi } from "@/services/setupAxios";
 // import Dashboard from "@/components/dashboard/dashboard";
 // import { BoardProvider } from '@/components/dashboard/context';
 
 const API = process.env.NEXT_PUBLIC_API || "http://65.108.79.164:3007/api";
 
 const ClassesDashboard = () => {
-    const [boardsData, setBoardsData] = useState();
+    // const [boardsData, setBoardsData] = useState();
 
-    useEffect(() => {
-        try {
-            instanceCoreApi.get(`${API}/classes?groupBy=semester`).then((res) => {
-                let classes = [];
-                let columns = [];
-                res.data.data.map((semester, id) => {
-                    semester._classes.map(item => {
-                        classes.push(item);
-                    })
-                    columns.push({
-                        "name": semester.semester,
-                        "color": "#123456",
-                        "items": semester._classes.map(item => item._id)
-                    })
-                })
-                data.boards.classes.items = classes;
-                data.boards.classes.columns = columns;
-                setBoardsData(data);
+    // useEffect(() => {
+    //     try {
+    //         instanceCoreApi.get(`${API}/classes?groupBy=semester`).then((res) => {
+    //             let classes = [];
+    //             let columns = [];
+    //             res.data.data.map((semester, id) => {
+    //                 semester._classes.map(item => {
+    //                     classes.push(item);
+    //                 })
+    //                 columns.push({
+    //                     "name": semester.semester,
+    //                     "color": "#123456",
+    //                     "items": semester._classes.map(item => item._id)
+    //                 })
+    //             })
+    //             data.boards.classes.items = classes;
+    //             data.boards.classes.columns = columns;
+    //             setBoardsData(data);
 
-            })
-        }
-        catch (err) {
-            console.error(err);
-        }
-    }, []);
+    //         })
+    //     }
+    //     catch (err) {
+    //         console.error(err);
+    //     }
+    // }, []);
 
-    console.log(boardsData)
+    // console.log(boardsData)
     return (
         <Layout pageTitle="Classes | CNWeb">
             {/* <div className="dashboard bg-[#212121] h-screen bg-center bg-cover bg-no-repeat flex items-center">
--- web/ui/pages/dashboard/quizzes.js
+++ web/ui/pages/dashboard/quizzes.js
@@ -1,10 +1,10 @@
 import Link from "next/link";
 import Layout from "@/components/layout";
-import Menu from "@/components/dashboard/menu";
+// import Menu from "@/components/dashboard/menu";
 
-import data from "@/assets/data/dashboard.json";
+// import data from "@/assets/data/dashboard.json";
 import React, { useState, useEffect } from "react";
-import { instanceCoreApi } from "@/services/setupAxios";
+// import { instanceCoreApi } from "@/services/setupAxios";
 // import Dashboard from "@/components/dashboard/dashboard";
 // import { BoardProvider } from '@/components/dashboard/context';
 
@@ -12,28 +12,28 @@ const API = process.env.NEXT_PUBLIC_API || "http://65.108.79.164:3007/api";
 
 const QuizzesDashboard = () => {
 
-    useEffect(() => {
-        try {
-            instanceCoreApi.get(`${API}/quizzes/`).then((res) => {
-                data.boards.quizzes.items = res.data.data;
-                data.boards.quizzes.columns.map((column, i) => {
-                    let temp = [];
-                    res.data.data.map((item, j) => {
-                        if (column.name === item.status) {
-                            temp.push(item._id);
-                        }
-                    })
-                    column.items = temp;
-                })
-                setBoardsData(data);
-            })
-        }
-        catch (err) {
-            console.error(err);
-        }
-    }, [])
+    // useEffect(() => {
+    //     try {
+    //         instanceCoreApi.get(`${API}/quizzes/`).then((res) => {
+    //             data.boards.quizzes.items = res.data.data;
+    //             data.boards.quizzes.columns.map((column, i) => {
+    //                 let temp = [];
+    //                 res.data.data.map((item, j) => {
+    //                     if (column.name === item.status) {
+    //                         temp.push(item._id);
+    //                     }
+    //                 })
+    //                 column.items = temp;
+    //             })
+    //             setBoardsData(data);
+    //         })
+    //     }
+    //     catch (err) {
+    //         console.error(err);
+    //     }
+    // }, [])
 
-    console.log(boardsData)
+    // console.log(boardsData)
     return (
         <Layout pageTitle="Quizzes | CNWeb">
             {/* <div className="dashboard bg-[#212121] h-screen bg-center bg-cover bg-no-repeat flex items-center">
