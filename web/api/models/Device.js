0cc64f7158496e11cea0a7ef3d77f30523ee912e
Master Pi <minh.vln140501@gmail.com>
2023-11-27 11:38:48 +0700

    device model

--- web/api/models/Device.js
+++ web/api/models/Device.js
@@ -2,9 +2,15 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const deviceSchema = new Schema({
-    name: {
+    id: {
         type: String,
         default: "epd#1"
+    },
+    ssid: {
+        type: String,
+    },
+    pass: {
+        type: String,
     },
     userID: {
         type: String,
